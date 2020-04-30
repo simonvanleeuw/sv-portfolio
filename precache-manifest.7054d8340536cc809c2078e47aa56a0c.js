9de0fca362c74e89eca05e5ae9d90e991aadde62
SimonVL01 <simon-vanleeuw@hotmail.com>
2020-04-30 15:37:05 +0200

    Updates

---
+++ precache-manifest.7054d8340536cc809c2078e47aa56a0c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df2e67eeb234ed6b053d968965a4146",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/index.html"
+  },
+  {
+    "revision": "78855cfc00445b14e555",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/static/css/main.6e163fa9.chunk.css"
+  },
+  {
+    "revision": "1982f9ad6ee07a682142",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/static/js/2.a2948eaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/static/js/2.a2948eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78855cfc00445b14e555",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/static/js/main.7d92784c.chunk.js"
+  },
+  {
+    "revision": "f103e323a2b506d26366",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/static/js/runtime-main.323d27ad.js"
+  },
+  {
+    "revision": "3daeafae31a19bf2f69c4d675c4dcea7",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/static/media/linkedin.3daeafae.svg"
+  },
+  {
+    "revision": "33734e6a1685b7c6e4f8ca62c427c3f1",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/static/media/menu.33734e6a.svg"
+  },
+  {
+    "revision": "74bddf1a83cedbed5ec94858aaa39959",
+    "url": "//simonvanleeuw.github.io/sv-portfolio/static/media/x.74bddf1a.svg"
+  }
+]);
